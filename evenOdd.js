4b30bfd12acb30e4b8e1900c25e2af22e0af1a54
divyanshisoni615 <divyanshisoni615@gmail.com>
2025-11-25 21:09:11 +0530

    javascript

---
+++ evenOdd.js
@@ -0,0 +1,12 @@
+// Input a number and print "Even" or "Odd".
+let a = Number(prompt("Enter a number:"));
+
+function checkEvenOdd(a) {
+    if (a % 2 == 0) {
+        console.log("even no.");
+    }
+    else {
+        console.log("odd no.");
+
+    }
+}
